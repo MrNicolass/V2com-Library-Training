2ee36f6a8a9c547a93b9540420c866bf21258ae2
Nicolas Gustavo Conte <nicolascontegt@gmail.com>
2025-02-04 10:10:44 -0300

    Project init
    - Basic structure of Quarkus;
    - There's libs for PostgresSQL JDBC, API REST and Panache.

---
+++ src/test/java/com/v2com/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.v2com;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
